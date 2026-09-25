9f85d27a08ad9e2ca26cf84df8b6ffb69fa95a39
= <brochettes40@outlook.fr>
2026-09-25 14:23:23 +0200

    feat : creat all respository

---
+++ backend/src/Repository/TodoListRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\TodoList;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<TodoList>
+ */
+class TodoListRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, TodoList::class);
+    }
+
+    
+}
